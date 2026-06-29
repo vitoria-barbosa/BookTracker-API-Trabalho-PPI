53310a94c4407b5ca0a91f478f28ac16869a7168
vitoria <vitoriabf2006@gmail.com>
2026-06-29 10:29:29 -0300

    :wrench:chore: adiciona url do bd para produção.

--- src/database.ts
+++ src/database.ts
@@ -4,11 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
+  connectionString: process.env.DATABASE_URL,
 });
 
 pool.connect((err, client, release) => {
